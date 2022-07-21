a706a551c273c364fd142aec9502619edef350d3
delaquash <olaide1191@gmail.com>
2022-07-21 11:36:42 +0100

    Fixed user schema

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const  mongoose  = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema ({
+    name :{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String
+    },
+    subscribers: {
+        type: Number,
+        default: 0
+    },
+    subscribedUsers: {
+        type: [String]
+    }
+},
+    {timestamps: true}
+)
+
+/*  */
+// module.exports = mongoose.model('User', UserSchema);
+
+export default mongoose.model("User", UserSchema)
