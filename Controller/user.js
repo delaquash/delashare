f56d285af13939411b8efd7f8a54013bbb5b3206
delaquash <olaide1191@gmail.com>
2022-07-27 13:01:10 +0100

    implemented update user

--- Controller/user.js
+++ Controller/user.js
@@ -1,7 +1,23 @@
-import User from "../models/User";
+import { createError } from "../error.js";
+import User from "../models/User.js";
 
-export const updateUser = async (req, res)=> {
-    const newUser = await User.findByIdAndUpdate(req.body.)
+
+export const updateUser = async (req, res, next)=> {
+    if(req.params.id === req.user.id){
+        try {
+            const updateUser = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body
+            }, {
+                new: true
+            });
+            res.status(200).json(updateUser)
+        } catch (error) {
+            next(err)
+        }
+    } else {
+        return next(createError(403, "You can update only your account!"))
+    }
+    
 };
 export const deleteUser = (req, res)=> {
     res.send("All is working")
