744d3c3ffec11dfca92260e153db5b7e5caddb86
delaquash <olaide1191@gmail.com>
2022-07-28 10:16:14 +0100

    implemeneted video update route

--- Controller/video.js
+++ Controller/video.js
@@ -12,8 +12,27 @@ export const addVideo = async (req, res, next)=> {
     }
 };
 export const updateVideo = async (req, res, next)=> {
-    
+    try {
+       /* Finding the video by the id and assigning it to the variable Video. */
+       const Video = await Video.findById(req.params.id) 
+        if(!video)next(createError(404, "Video not found!"))
+        if(req.user.id === video.userId){
+                const updateVideo = await User.findByIdAndUpdate(req.params.id, {
+                    $set: req.body
+                }, {
+                    new: true
+                });
+                res.status(200).json(updateVideo)
+             
+        } else {
+            return next(createError(403, "You can update only your video!!"))
+        }
+    } catch(err) {
+        next(err)
+    }
 };
+
+
 export const getVideo = async (req, res, next)=> {
     res.send("All is working")
 };
--- Routes/Video.js
+++ Routes/Video.js
@@ -5,7 +5,11 @@ const router = express.Router()
 
 router.post('/', verifyToken, addVideo )
 router.put('/:id', verifyToken, updateVideo )
+router.put('/view/:id', verifyToken, viewideo )
 router.get('/find/:id' , getVideo )
+router.get('/trend' , trendVideo )
+router.get('/random' , randomVideo )
+router.get('/sub' , subVideo )
 router.delete('/:id', verifyToken, deleteVideo )
 
 export default router; 
