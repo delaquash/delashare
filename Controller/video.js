bc411d9f38bd04ad1d04d71ff586695ffce5a21a
delaquash <olaide1191@gmail.com>
2022-07-28 15:19:01 +0100

    Implemented get specific video route

--- Controller/video.js
+++ Controller/video.js
@@ -15,7 +15,7 @@ export const updateVideo = async (req, res, next)=> {
     try {
        /* Finding the video by the id and assigning it to the variable Video. */
        const video = await Video.findById(req.params.id) 
-        if(!video)next(createError(404, "Video not found!"))
+        if(!video) return next(createError(404, "Video not found!"))
         if(req.user.id === video.userId){
                 const updatedVideo = await Video.findByIdAndUpdate(req.params.id, {
                     $set: req.body
@@ -23,23 +23,41 @@ export const updateVideo = async (req, res, next)=> {
                 {
                     new: true
                 });
-                res.status(200).json(updatedVideo)
-             
-        } 
+                res.status(201).json(updatedVideo)     
+        } else {
+            return next(createError(403, "You can update only your video"))
+        }
     } catch(err) {
         next(err)
     }
 }
 
 export const getVideo = async (req, res, next)=> {
-    res.send("All is working")
+    try {
+        const video = await Video.findById(req.params.id)
+        // const video = await Video.find()
+        res.status(200).json(video)
+    } catch (err) {
+        next(err)
+    }
 };
+
+export const addView = async(req, res, next)=> {
+    try {
+        await Video.findByIdAndUpdate(req.params.id, {
+            $inc: {views: 1}
+        })
+        res.status(201).json("Views has been increased successfully!")
+    } catch (err) {
+        next(err)
+    }
+}
 
 export const deleteVideo = async (req, res, next)=> {
     try {
        /* Finding the video by the id and assigning it to the variable Video. */
-       const video = await Video.findById(req.params.id) 
-         if(!video)next(createError(404, "Video not found!"))
+       const video = await Video.findById(req.params.id);
+         if(!video) return next(createError(404, "Video not found!"))
 
          /* Checking if the user is the owner of the video. */
          if(req.user.id === video.userId){
