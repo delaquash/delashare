c4fa22eed9d91a841803f6cf79a9484234058955
delaquash <olaide1191@gmail.com>
2022-08-01 14:30:29 +0100

    Fixed delete comment route

--- Controller/comment.js
+++ Controller/comment.js
@@ -12,23 +12,23 @@ export const addComment = async (req, res)=> {
         next(err)
     }
 };
-// export const deleteComment = async (req, res)=> {
-//     try {
-//         /* Finding the video by the id and assigning it to the variable Video. */
-//        const comment = await Comment.findById(req.params.id);
-//        const video = await Video.findById(req.params.id);
-//        if(!comment) return next(createError(404, "Comment not found!"));
+export const deleteComment = async (req, res)=> {
+    try {
+        /* Finding the video by the id and assigning it to the variable Video. */
+       const comment = await Comment.findById(req.params.id);
+       const video = await Video.findById(req.params.id);
+       if(!comment) return next(createError(404, "Comment not found!"));
 
-//        if (req.user.id === comment.userId || req.user.id === video.userId){
-//         await Comment.findByIdAndDelete(req.params.id)
-//         res.status(200).json("Comment has been deleted successfully!");
-//        }else {
-//             return next(createError(403, "You can only delete your video"))
-//        }
-//     } catch (err) {
-//         next(err)
-//     }
-// };
+       if (req.user.id === comment.userId || req.user.id === video.userId){
+        await Comment.findByIdAndDelete(req.params.id)
+        res.status(200).json("Comment has been deleted successfully!");
+       }else {
+            return next(createError(403, "You can only delete your video"))
+       }
+    } catch (err) {
+        next(err)
+    }
+};
 export const getComment = async (req, res)=> {
     try {
        const comment = await Comment.findById({video: req.params.videoId}) 
