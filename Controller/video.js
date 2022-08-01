81cacc13ab0453adbf788ef77530312597708be5
delaquash <olaide1191@gmail.com>
2022-08-01 11:53:56 +0100

    fixed search by tags query route

--- Controller/video.js
+++ Controller/video.js
@@ -12,29 +12,59 @@ export const addVideo = async (req, res, next)=> {
         next(err);
     }
 };
-export const updateVideo = async (req, res, next)=> {
-    try {
-       /* Finding the video by the id and assigning it to the variable Video. */
-       const video = await Video.findById(req.params.id) 
-        if(!video) return next(createError(404, "Video not found!"));
-        if(req.user.id === video.userId){
+// export const updateVideo = async (req, res, next)=> {
+//     try {
+//        /* Finding the video by the id and assigning it to the variable Video. */
+//        const video = await Video.findById(req.params.id) 
+//         if(!video) return next(createError(404, "Video not found!"));
+//         if(req.user.id === video.userId){
     
-                const updatedVideo = await Video.findByIdAndUpdate(
-                    req.params.id, 
-                    {
-                        $set: req.body,                   }, 
-                    {
-                        new: true
-                    }
-                );
-                res.status(200).json(updatedVideo)     
-            } else {
-            return next(createError(403, "You can update only your video"))
-        }
-    } catch(err) {
-        next(err)
+//                 const updatedVideo = await Video.findByIdAndUpdate(
+//                     req.params.id, 
+//                     {
+//                         $set: req.body,                   }, 
+//                     {
+//                         new: true
+//                     }
+//                 );
+//                 res.status(200).json(updatedVideo)     
+//             } else {
+//             return next(createError(403, "You can update only your video"))
+//         }
+//     } catch(err) {
+//         next(err)
+//     }
+// };
+
+export const updateVideo = async (req, res, next) => {
+    try {
+      const video = await Video.findById(req.params.id);
+      if (!video) return next(createError(404, "Video not found!"));
+      if (req.user.id === video.userId) {
+        const updatedVideo = await Video.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedVideo);
+      } else {
+        return next(createError(403, "You can update only your video!"));
+      }
+    } catch (err) {
+      next(err);
     }
-};
+  };
+
+
+
+
+
+
+
+
+
 
 export const getVideo = async (req, res, next)=> {
     try {
@@ -114,4 +144,19 @@ export const deleteVideo = async (req, res, next)=> {
      } catch(err) {
          next(err)
      }
+}
+
+export const tags = async (req, res)=> {
+    const tags = req.query.tags.split(",");
+    // console.log(tags);
+    try {
+        const videos = await Video.find({ tags: {$in: tags}}).limit(20)
+        res.status(200).json(videos)
+    } catch (err) {
+        next(err)
+    }
+};
+
+export const search = (req, res)=> {
+
 }
