803a1d184b46eaf0d3dd3747cce5e4f0c9eb4885
delaquash <olaide1191@gmail.com>
2022-08-10 13:35:59 +0100

    Fixed redux store for user login

--- client/src/Redux/userSlice.js
+++ client/src/Redux/userSlice.js
@@ -1,15 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    user: null,
+    currentUser: null,
     loading: false,
     error: false
 }
 
 export const userSlice = createSlice({
-    name: "user", 
+    name: "currentUser", 
     initialState, 
     reducers: {
-        
+        loginStart:(state)=> {
+            /* Setting the loading property of the state to true. */
+            state.loading= true;
+        },
+        loginSuccess:(state,action) => {
+            state.loading = false;
+            state.currentUser= action.payload
+        },
+        loginFail: (state) => {
+            state.loading = false;
+            state.error = true
+        },
+        logOut:(state) => {
+            return initialState;
+        }
     }
 })
+
+export const { loginStart, loginFail, loginSuccess, logOut } = userSlice.actions
+
+export default userSlice.reducer
