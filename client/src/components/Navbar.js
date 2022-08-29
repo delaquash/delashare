a3f424bd15c55915af4e030b620d7995b4822608
delaquash <olaide1191@gmail.com>
2022-08-29 11:26:16 +0100

    Fixed video upload modal

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,6 +6,7 @@ import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import VideoCallOutlinedAction from "@mui/icons-material/VideoCallOutlined";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import Upload from "./Upload";
 
 const Container = styled.div`
   position: sticky;
@@ -76,29 +77,29 @@ const Navbar = () => {
   const { currentUser } = useSelector(state => state.user )
   return (
     <>
-    <Container>
-      <Wrapper>
-        <Search>
-          <Input placeholder="Search" />
-          <SearchOutlinedIcon />
-        </Search>
-       { currentUser ? (
-          <User>
-            <VideoCallOutlinedAction onClick={} />
-            <Avatar src={currentUser.img} />
-            {currentUser.name}
-          </User>
-       ) :(
-       <Link to="signin" style={{ textDecoration: "none" }}>
-          <Button>
-            <AccountCircleOutlinedIcon />
-            SIGN IN
-          </Button>
-        </Link>)}
-      </Wrapper>
-    </Container>
+      <Container>
+        <Wrapper>
+          <Search>
+            <Input placeholder="Search" />
+            <SearchOutlinedIcon />
+          </Search>
+        { currentUser ? (
+            <User>
+              <VideoCallOutlinedAction onClick={() => setOpen(true)} />
+              <Avatar src={currentUser.img} />
+              {currentUser.name}
+            </User>
+        ) :(
+        <Link to="signin" style={{ textDecoration: "none" }}>
+            <Button>
+              <AccountCircleOutlinedIcon />
+              SIGN IN
+            </Button>
+          </Link>)}
+        </Wrapper>
+      </Container>
+    {open && <Upload setOpen={setOpen} />}
     </>
-    {open && }
   );
 };
 
--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import styled from 'styled-components';
 
-const Upload = () => {
+const Container = styled.div`
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: #000000af;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+const Wrapper = styled.div`
+    width: 600px;
+    height: 600px;
+    display: flex;
+    position: relative;
+    gap: 20px;
+    flex-direction: column;
+    padding: 20px;
+    color: ${({ theme}) => theme.text};
+    background-color: ${({theme}) => theme.bgLighter };
+`;
+const Close = styled.button`
+    position: absolute;
+    top: 10px;
+    cursor: pointer;
+    right: 10px;
+`;
+const Title = styled.h1`
+    
+`;
+
+const Upload = ({setOpen}) => {
   return (
-    <div>Upload</div>
+    <Container>
+        <Wrapper>
+            <Close onClick={()=>setOpen(false)}>X</Close>
+            <Title>Upload</Title>
+        </Wrapper>
+    </Container>
   )
 }
 
